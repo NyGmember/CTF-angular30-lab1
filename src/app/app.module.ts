4abc1c93f0d6cc6d7f9895ee0ba144ae6d29bd73
NyGmember <nygmember.002@gmail.com>
2021-04-24 16:14:49 +0700

    Add app-routers material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,55 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CatalogModule } from "./catalog/catalog.module";
 import { HomeModule } from "./home/home.module";
-import { RouterModule, Routes } from "@angular/router";
+import { AppRouteModule } from "./app-route.module";
+import { MatButtonModule } from "@angular/material/button";
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from "./home/home.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ProductListComponent } from './catalog/product-list/product-list.component';
-import { ProductDetailComponent } from './catalog/product-detail/product-detail.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'home'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'products',
-    component: ProductListComponent
-  },
-  {
-    path: 'products/:id',
-    component: ProductDetailComponent
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent
-  }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     CatalogModule,
     HomeModule,
-    RouterModule.forRoot(routes)
+    AppRouteModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
